d19bfe3607280f216a338e3ab5fc82496f2922a1
Nicolas <nicolaswow3@hotmail.com>
2024-07-08 15:47:53 -0600

    Update live-chat.component.ts

--- src/app/components/live-chat/live-chat.component.ts
+++ src/app/components/live-chat/live-chat.component.ts
@@ -27,8 +27,8 @@ export class LiveChatComponent implements OnInit {
     //Idealmente esto estaría implementado con un sistema de autenticación y cada usuario de la aplicación tendría asignado un JWT token generado con el server client de Stream Chat.
     //Esta implementación demo simula un canal al que pueden entrar varios usuarios y enviar/recibir mensajes. Claramente por demostración solo está un usuario dentro del canal.
     const apiKey = 'n4zhq2zcd4fu';
-    const userId = 'viento-nica-1';
-    const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidmllbnRvLW5pY2EtMSJ9.2QdeaZ2fpAa79fSZFpoIDkuds3SNumMyKKVDCas-mmo';
+    const userId = 'viento-peruano-1';
+    const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidmllbnRvLXBlcnVhbm8tMSJ9.tRCT7cbu-QuDb2AjyEEgaX-33U0TJR3RXw5pHGVWNkM';
     this.chatService.init(apiKey, userId, userToken);
     this.streamI18nService.setTranslation();
   }
@@ -40,6 +40,10 @@ export class LiveChatComponent implements OnInit {
       name: 'Excuela - Conocimiento Compartido',
     });
     await channel.create();
+
+    //Añado dos miembros de prueba al canal. Junto con el repositorio del proyecto estaré enviando un link a una instancia online del mismo proyecto pero con viento-nica-1 como
+    //usuario por defecto para que cuando corran de manera local el proyecto puedan probar el chat en conjunto con el proyecto online.
+    await channel.addMembers([{ user_id: "viento-peruano-1", channel_role: "channel_moderator" }, { user_id: "viento-nica-1", channel_role: "channel_moderator" }]);
 
     //Se añade condición para filtrar canales al inicializar el channelService, en este caso estamos filtrando por el ID que le dimos al canal cuando fue creado 'conocimiento-excuela'.
     this.channelService.init({
